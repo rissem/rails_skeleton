a9b2879ab7b5fa393d3c7084f6c08a93e015ca60
Mike Risse <rissem@gmail.com>
2012-06-02 11:40:42 -0700

    add our api keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :singly, ENV['SINGLY_ID'], ENV['SINGLY_SECRET']
+  provider :singly, "ae589cd8c2a589b0381715eb73c25b2a", "91a16fba55639bc91b1d6233e83bca35"
+#  provider :singly, ENV['SINGLY_ID'], ENV['SINGLY_SECRET']
 end

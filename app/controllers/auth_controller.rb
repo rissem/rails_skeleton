88661537fa94bb0dad37b5a2b181bd7071b312ff
Mike Risse <rissem@gmail.com>
2012-06-03 10:44:04 -0700

    add back code to dump third party info to db

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -23,20 +23,20 @@ class AuthController < ApplicationController
     user = User.new unless user
     user.access_token = access_token
     profiles = HTTParty.get(profiles_url, :query => {:access_token => access_token}).parsed_response
-    # if profiles["facebook"] and not user.fb_info
-    #   user.fb_info = HTTParty.get("#{SINGLY_API_BASE}/v0/services/facebook/self", :query => {:access_token => access_token}).parsed_response[0].to_json
-    #   user.fb_contacts = HTTParty.get("#{SINGLY_API_BASE}/v0/services/facebook/friends", :query => {:access_token => access_token}).parsed_response.to_json
-    # end
+    if profiles["facebook"] and not user.fb_info
+      user.fb_info = HTTParty.get("#{SINGLY_API_BASE}/v0/services/facebook/self", :query => {:access_token => access_token}).parsed_response[0].to_json
+      user.fb_contacts = HTTParty.get("#{SINGLY_API_BASE}/v0/services/facebook/friends", :query => {:access_token => access_token}).parsed_response.to_json
+    end
 
-    # if profiles["twitter"] and not user.twitter_info
-    #   user.twitter_info =  HTTParty.get("#{SINGLY_API_BASE}/v0/services/twitter/self", :query => {:access_token => access_token}).parsed_response[0].to_json
-    #   user.twitter_contacts = HTTParty.get("#{SINGLY_API_BASE}/v0/services/twitter/friends", :query => {:access_token => access_token}).parsed_response.to_json
-    # end
+    if profiles["twitter"] and not user.twitter_info
+      user.twitter_info =  HTTParty.get("#{SINGLY_API_BASE}/v0/services/twitter/self", :query => {:access_token => access_token}).parsed_response[0].to_json
+      user.twitter_contacts = HTTParty.get("#{SINGLY_API_BASE}/v0/services/twitter/friends", :query => {:access_token => access_token}).parsed_response.to_json
+    end
 
-    # if profiles["linkedin"] and not user.linkedin_info
-    #   user.linkedin_info = HTTParty.get("#{SINGLY_API_BASE}/v0/services/linkedin/self", :query => {:access_token => access_token}).parsed_response[0].to_json
-    #   user.linkedin_contacts = HTTParty.get("#{SINGLY_API_BASE}/v0/services/linkedin/connectins", :query => {:access_token => access_token}).parsed_response.to_json
-    # end
+    if profiles["linkedin"] and not user.linkedin_info
+      user.linkedin_info = HTTParty.get("#{SINGLY_API_BASE}/v0/services/linkedin/self", :query => {:access_token => access_token}).parsed_response[0].to_json
+      user.linkedin_contacts = HTTParty.get("#{SINGLY_API_BASE}/v0/services/linkedin/connectins", :query => {:access_token => access_token}).parsed_response.to_json
+    end
     user.save!
   end
 
